40c294918a2db2ef56c83c215f4bf9fe1f2a2d7a
mmahadt <14besemmahad@seecs.edu.pk>
2016-12-05 18:33:30 +0500

    feat: Initialize sleep_until property of struct thread

--- threads/thread.c
+++ threads/thread.c
@@ -103,6 +103,7 @@ thread_init (void)
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
+  initial_thread->sleep_until = 0;
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
